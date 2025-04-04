885bf33397265abe6128fca7e701515d581aa337
i.shablovskiy <ilya.shablovskiy@gmail.com>
2025-04-04 21:20:19 +0300

    feat: create product form

---
+++ src/components/ProductForm/ProductForm.tsx
@@ -0,0 +1,49 @@
+import { useState } from 'react';
+import { useForm } from 'react-hook-form';
+
+import { useProductStore } from '@/lib/store/productStore';
+
+interface ProductFormData {
+  name: string;
+  price: number;
+  image: string;
+}
+
+const ProductForm = () => {
+  const { register, handleSubmit, reset } = useForm<ProductFormData>();
+  //ToDo: create action creator?
+  const addProduct = useProductStore((state) => state.addProduct);
+  const [isOpen, setIsOpen] = useState(false);
+
+  const onSubmit = (data: ProductFormData) => {
+    addProduct(data);
+    reset();
+    setIsOpen(false);
+  };
+
+  return (
+    <>
+      <button onClick={() => setIsOpen(true)}>Add Product</button>
+      {isOpen && (
+        //ToDo: use portal instead of div
+        <div className="modal">
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <input {...register('name', { required: true })} placeholder="Name" />
+            <input
+              {...register('price', { required: true, valueAsNumber: true })}
+              type="number"
+              placeholder="Price"
+            />
+            <input {...register('image', { required: true })} placeholder="Image URL" />
+            <button type="submit">Add</button>
+            <button type="button" onClick={() => setIsOpen(false)}>
+              Cancel
+            </button>
+          </form>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default ProductForm;
