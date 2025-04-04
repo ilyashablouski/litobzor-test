66ec56e972a45d051771d3146fbaa6ef37e4354c
i.shablovskiy <ilya.shablovskiy@gmail.com>
2025-04-04 20:25:41 +0300

    chore: add products mock data

---
+++ src/data/products.ts
@@ -0,0 +1,5 @@
+export const initialProducts = [
+  { id: 1, name: 'Laptop', price: 1500, image: 'https:/https://placehold.co/150' },
+  { id: 2, name: 'Mouse', price: 500, image: 'https:/https://placehold.co/150' },
+  { id: 3, name: 'Keyboard', price: 800, image: 'https:/https://placehold.co/150' },
+];
