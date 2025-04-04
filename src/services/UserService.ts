5c59fd411cc3503c27f72bd904ced4654f3e5aaa
i.shablovskiy <ilya.shablovskiy@gmail.com>
2025-04-04 20:41:40 +0300

    feat: create UserService to fetch users from jsonplaseholder

---
+++ src/services/UserService.ts
@@ -0,0 +1,17 @@
+import axios from 'axios';
+
+import { User } from '@/lib/types/user';
+
+export class UserService {
+  private static readonly API_URL = 'https://jsonplaceholder.typicode.com/users';
+
+  static async fetchUsers(): Promise<User[]> {
+    try {
+      const response = await axios.get<User[]>(this.API_URL);
+      return response.data;
+    } catch (error) {
+      console.error('Error fetching users:', error);
+      return [];
+    }
+  }
+}
