b6b004578537b99dcfcc8704b73663b4addea7c4
i.shablovskiy <ilya.shablovskiy@gmail.com>
2025-04-04 20:14:53 +0300

    chore: add saas setup

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,13 @@
+import path from 'path';
+
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'src/styles')],
+    additionalData: `@use "services.scss" as *;`,
+  },
+
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
