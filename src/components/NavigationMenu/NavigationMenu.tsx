efeb1d488328b5f63828b1b6ca01307f9a93588f
i.shablovskiy <ilya.shablovskiy@gmail.com>
2025-04-05 17:47:25 +0300

    feat: add nav menu styles & hover logic

--- src/components/NavigationMenu/NavigationMenu.tsx
+++ src/components/NavigationMenu/NavigationMenu.tsx
@@ -1,10 +1,27 @@
+'use client';
+
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+
+import styles from './NavigationMenu.module.scss';
 
 const NavigationMenu = () => {
+  const pathname = usePathname();
+
   return (
-    <nav className="navigation">
-      <Link href="/products">Products</Link>
-      <Link href="/users">Users</Link>
+    <nav className={styles.navigation}>
+      <Link
+        href="/products"
+        className={`${styles.navigationLink} ${pathname === '/products' || pathname === '/' ? styles.active : ''}`}
+      >
+        Products
+      </Link>
+      <Link
+        href="/users"
+        className={`${styles.navigationLink} ${pathname === '/users' ? styles.active : ''}`}
+      >
+        Users
+      </Link>
     </nav>
   );
 };
